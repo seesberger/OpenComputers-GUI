b96d823f77ef8cd2598992cf77950ef35c64df19
Frederick Meisel <meisel.frederick@gmx.de>
2024-02-28 10:30:51 +0100

    reworked dependency coption

---
+++ dependencies.lua
@@ -0,0 +1,19 @@
+--This file can be used by adding it as a lib in Git-tool:
+--require = ("dependencies")
+--to run the install script call dependencies:installScript([folder used for storing temporary files], <Library folder; default: /lib>)
+
+--The entries shoud be accepted by just adding them. Just look at the template how to add things.
+
+dependencies = {
+}
+
+return dependencies
+
+--[[
+    Template:
+    dep1 = {
+        Name = "",
+        Url = "",
+        InstallTarget = ""
+    },
+]]
