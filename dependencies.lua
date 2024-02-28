4e2aa1ca3a4c3550515da4d3b77fc2612aac285c
Frederick Meisel <meisel.frederick@gmx.de>
2024-02-28 11:24:02 +0100

    Added Install script for startup

--- dependencies.lua
+++ dependencies.lua
@@ -5,6 +5,20 @@
 --The entries shoud be accepted by just adding them. Just look at the template how to add things.
 
 dependencies = {
+    dep1 = {
+    Name = "",
+    Url = "",
+    InstallTarget = ""
+    },
+    --Not the best thing at the moment, but can be used to move things arround
+    dependencyInstall = {
+        Name = "Script",
+        InstallScript = function(temporaryPath, libPath)
+            libPath = libPath or "/lib/"
+            os.execute("mkdir "..libPath.."FormatModules")--not shure if useless, keeping for redundancy
+            os.execute("cp -r "..temporaryPath.."* "..libPath)
+        end
+    }
 }
 
 return dependencies
@@ -16,4 +30,10 @@ return dependencies
         Url = "",
         InstallTarget = ""
     },
+    dependencyInstall = {
+        Name = "Script",
+        InstallScript = function(temporaryPath, targetPath)
+            os.execute("")
+        end
+    }
 ]]
