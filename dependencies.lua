035623447d045961b7c3473def60f62e1333ec5a
Frederick Meisel <meisel.frederick@gmx.de>
2024-02-28 15:24:37 +0100

    Added custom window constructor

--- dependencies.lua
+++ dependencies.lua
@@ -1,22 +1,14 @@
 --This file can be used by adding it as a lib in Git-tool:
---require = ("dependencies")
---to run the install script call dependencies:installScript([folder used for storing temporary files], <Library folder; default: /lib>)
-
---The entries shoud be accepted by just adding them. Just look at the template how to add things.
+--require = ("dependencies") -> already implemented in InstallDependencies
+--If a a Table is given the Name "Script" it will automatically run the contained Function "InstallScript" while installing dependencies
 
 dependencies = {
-    dep1 = {
-    Name = "",
-    Url = "",
-    InstallTarget = ""
-    },
     --Not the best thing at the moment, but can be used to move things arround
     dependencyInstall = {
         Name = "Script",
         InstallScript = function(temporaryPath, libPath)
-            libPath = libPath or "/lib/"
-            os.execute("mkdir "..libPath.."FormatModules")--not shure if useless, keeping for redundancy
-            os.execute("cp -r "..temporaryPath.."* "..libPath)
+            --FIXME: Recursively call git tool to install automatically from repo!
+            print("Please make shure you have installed the Seesberger/OpenComputers-GUI")
         end
     }
 }
